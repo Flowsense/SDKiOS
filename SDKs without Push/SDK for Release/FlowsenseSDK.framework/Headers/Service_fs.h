655cfbbe473fc51539abd663c625f5ee2be7f4fd
Goldenberg <rafaelgo@kth.se>
2017-04-10 14:48:17 -0300

    version 2.0.5

--- SDKs without Push/SDK for Release/FlowsenseSDK.framework/Headers/Service_fs.h
+++ SDKs without Push/SDK for Release/FlowsenseSDK.framework/Headers/Service_fs.h
@@ -6,6 +6,7 @@
 +(void) StartFlowsenseService:(NSString *)partnerToken :(BOOL) startNow;
 //+(void) StartFlowsensePushService:(NSDictionary *) launchOptions;
 +(void) StartMonitoringLocation;
++(void) StartMonitoringLocationReact;
 //+(void) postToken:(NSData *)token;
 +(void) PostJsonCreate;
 +(void) updatePartnerUserIdiOS:(NSString *) userId;
