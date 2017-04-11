85284298285dc7c733462a531eb1da8081eb57f4
Goldenberg <rafaelgo@kth.se>
2017-04-11 18:04:47 -0300

    Created universal framework

---
+++ SDKs with Push Enabled/FlowsenseSDK.framework/Headers/Service_fs.h
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+
+@interface Service_fs : NSObject
+
++(void) StartFlowsenseService:(NSString *)partnerToken;
++(void) StartFlowsenseService:(NSString *)partnerToken :(BOOL) startNow;
+
++(void) StartFlowsensePushService:(NSDictionary *) launchOptions;
++(void) postToken:(NSData *)token;
++(void) sendPushToken:(NSString *)token;
+
++(void) StartMonitoringLocation;
++(void) StartMonitoringLocationReact;
++(void) PostJsonCreate;
++(void) updatePartnerUserIdiOS:(NSString *) userId;
++(NSString *) getSDKVersion;
+
++(void) updateGeofences;
++(NSArray *) getStoredGeofences;
++(NSArray *) getKeyValues;
+@end
