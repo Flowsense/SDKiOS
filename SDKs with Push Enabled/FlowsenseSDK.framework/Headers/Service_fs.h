61d6911292d7836ef0eb0945ca956666c2061af4
Goldenberg <rafaelgo@kth.se>
2017-10-09 14:48:19 -0300

    Cordova modifications

--- SDKs with Push Enabled/FlowsenseSDK.framework/Headers/Service_fs.h
+++ SDKs with Push Enabled/FlowsenseSDK.framework/Headers/Service_fs.h
@@ -1,11 +1,16 @@
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
 @interface Service_fs : NSObject
 
+NS_ASSUME_NONNULL_BEGIN
 +(void) StartFlowsenseService:(NSString *)partnerToken;
 +(void) StartFlowsenseService:(NSString *)partnerToken :(BOOL) startNow;
 
+//Push Services
 +(void) StartFlowsensePushService:(NSDictionary *) launchOptions;
++(void) includeMediaAttachmentWithRequest:(UNNotificationRequest *)request mutableContent:(UNMutableNotificationContent *)bestAttemptContent contentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler;
+
 +(void) sendPushToken:(NSData *)token;
 +(NSDictionary *) getPushExtras;
 
@@ -15,6 +20,7 @@
 +(void) updateGeofences;
 +(NSArray *) getStoredGeofences;
 +(NSArray *) getKeyValues;
+NS_ASSUME_NONNULL_END
 
 
 @end
--- SDKs without Push/FlowsenseSDK.framework/Headers/Service_fs.h
+++ SDKs without Push/FlowsenseSDK.framework/Headers/Service_fs.h
@@ -1,12 +1,17 @@
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
 @interface Service_fs : NSObject
 
+NS_ASSUME_NONNULL_BEGIN
 +(void) StartFlowsenseService:(NSString *)partnerToken;
 +(void) StartFlowsenseService:(NSString *)partnerToken :(BOOL) startNow;
 
+//Push Services
 //+(void) StartFlowsensePushService:(NSDictionary *) launchOptions;
-+(void) sendPushToken:(NSData *)token;
+//+(void) includeMediaAttachmentWithRequest:(UNNotificationRequest *)request mutableContent:(UNMutableNotificationContent *)bestAttemptContent contentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler;
+
+//+(void) sendPushToken:(NSData *)token;
 +(NSDictionary *) getPushExtras;
 
 +(void) StartMonitoringLocation;
@@ -15,6 +20,7 @@
 +(void) updateGeofences;
 +(NSArray *) getStoredGeofences;
 +(NSArray *) getKeyValues;
+NS_ASSUME_NONNULL_END
 
 
 @end
