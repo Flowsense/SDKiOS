d9974df48632f18d3244f65abe01f6b13d7c0b15
Goldenberg <rafaelgo@kth.se>
2018-10-05 11:47:40 -0300

    Revised Push Geofencing

--- SDKs with Push Enabled/FlowsenseSDK.framework/Headers/Service_fs.h
+++ SDKs with Push Enabled/FlowsenseSDK.framework/Headers/Service_fs.h
@@ -9,10 +9,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 //Push Services
 
+// Initializes Push Service
 +(void) StartFlowsensePushService:(NSDictionary *) launchOptions;
+
+// Initializes Push Service with action block allowing user to retrieve the push token and push actions: arrival and click
+// result is @{@"push_token": "token", @"push_received": <NSDictionary containing payload>, @"push_clicked": <NSDictionary containing payload>}
 +(void) StartFlowsensePushServiceWithLaunchOptions:(NSDictionary *) launchOptions actionBlock:(void(^)(NSDictionary *result))completionHandler;
+
+// Displays an image in Push
 +(void) includeMediaAttachmentWithRequest:(UNNotificationRequest *)request mutableContent:(UNMutableNotificationContent *)bestAttemptContent contentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler;
+
+// Set a Push callback when push is received and clicked with:
+// result is @{@"push_token": "token", @"push_received": <NSDictionary containing payload>, @"push_clicked": <NSDictionary containing payload>}
 +(void) setPushCallback:(void(^)(NSDictionary *result))completionHandler;
+
+// Open a deeplink with an app URI and, optionally, an Itunes Store link for app installation and the store app ID
++(void) openDeepLinkWithURI:(NSString *)appURI storeLink:(NSString *)itunesLink storeID:(NSNumber *)appID;
+
++(void) sendPushClickAnalytics:(NSString *)pushUUID;
++(void) sendPushArrivedAnalytics:(NSString *)pushUUID;
 
 +(void) sendPushToken:(NSData *)token;
 +(NSDictionary *) getPushExtras;
